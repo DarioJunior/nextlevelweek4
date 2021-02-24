6efa48de1a0818fdf309f04a8e58bc7e2350c62c
Dário Junior <dario.junior.rs@gmail.com>
2021-02-24 18:15:55 -0300

    Aula 3 [checked]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/global.css'; /* para todas as páginas */
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChallengesProvider>
+          <Component {...pageProps} />
+      </ChallengesProvider>
+  )
 }
 
 export default MyApp
